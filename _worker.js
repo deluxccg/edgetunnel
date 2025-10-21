3e4e8894d5a47d54e2d99ce5393ae4940d0b8846
deluxccg <48112782+deluxccg@users.noreply.github.com>
2025-10-21 09:33:11 +0800

    1
    1

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 ﻿
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '735134b6-5440-4343-9577-6f2260ac51da';
 let proxyIP = '';
 //let sub = '';
 let subConverter = atob('U3ViQXBpLkNtbGlVc3NzUy5OZXQ=');
@@ -5846,4 +5846,5 @@ async function 解析地址端口(proxyIP) {
         端口 = parseInt(proxyIP.slice(colonIndex + 1), 10) || 端口;
     }
     return [地址, 端口];
+
 }
